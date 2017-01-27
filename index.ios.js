7ddec1b2ef67c585cadbb8f0180262ed349e1d83
Stefan Wallin <stefan.w@festiz.com>
2017-01-27 14:00:11 +0100

    Minimal test case for RTL + lineheight

--- index.ios.js
+++ index.ios.js
@@ -7,24 +7,24 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  I18nManager,
+  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+I18nManager.forceRTL(true);
+
 export default class rtl_test_app extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          أحب اللغة العربية without lineHeight
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.instructions2}>
+          أحب اللغة العربية with lineHeight
         </Text>
       </View>
     );
@@ -37,13 +37,24 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    margin: 10,
-  },
   instructions: {
     color: '#333333',
     marginBottom: 5,
+    ...Platform.select({
+      ios: {
+        writingDirection: I18nManager.isRTL ? 'rtl' : 'ltr',
+      },
+    }),
+  },
+  instructions2: {
+    color: '#333333',
+    marginBottom: 5,
+    lineHeight: 20,
+    ...Platform.select({
+      ios: {
+        writingDirection: I18nManager.isRTL ? 'rtl' : 'ltr',
+      },
+    }),
   },
 });
 
